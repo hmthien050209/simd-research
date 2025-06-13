5c292add93735781adcd77cefd6c8af75904b623
hmthien050209 <hoangminhthien05022009@gmail.com>
2025-06-13 02:36:14 +0000

    feat: added Scorer::BooleanMultiplicationScorer

--- main.cpp
+++ main.cpp
@@ -1,7 +1,6 @@
 #include "exam/exam.h"
 #include "exec_timer/exec_timer.h"
 
-#include <assert.h>
 #include <iostream>
 #include <vector>
 
@@ -32,13 +31,19 @@ public:
   }
 };
 
-class MultithreadedNaiveScorer final : public BaseScorer {
+class BooleanMultiplicationScorer final : public BaseScorer {
+public:
   std::vector<uint32_t> score(const std::vector<Exam> &exams,
                               const Exam &correct_answers,
                               const std::vector<uint8_t> &points) override {
-    ExecTimer timer("MultithreadedNaiveScorer");
+    ExecTimer timer("BooleanMultiplicationScorer");
     std::vector<uint32_t> scored_exams_points(exams.size());
-    throw std::runtime_error("Not implemented");
+    for (size_t i = 0; i < exams.size(); ++i) {
+      for (size_t j = 0; j < exams[i].size(); ++j) {
+        scored_exams_points[i] +=
+            (exams[i][j] == correct_answers[j]) * points[j];
+      }
+    }
     return scored_exams_points;
   }
 };
@@ -53,23 +58,43 @@ int main() {
   // The scorers to be tested
   const std::vector<std::shared_ptr<Scorer::BaseScorer>> scorers = {
       std::make_shared<Scorer::NaiveScorer>(),
-      // std::make_shared<Scorer::BaseScorer>(Scorer::MultithreadedNaiveScorer()),
+      std::make_shared<Scorer::BooleanMultiplicationScorer>(),
   };
-  std::vector<std::vector<uint32_t>> results;
+  std::vector<std::vector<uint32_t>> scored_results;
   for (const auto &scorer : scorers) {
-    results.emplace_back(scorer->score(exams, correct_answers, points));
+    scored_results.emplace_back(scorer->score(exams, correct_answers, points));
   }
 
   // Comparison logic
-  assert(scorers.size() == results.size());
-  for (auto it = results.begin(); it != results.end() - 1; ++it) {
-    auto &result = *it;
-    auto &next = *(it + 1);
+  if (scorers.size() != scored_results.size()) {
+    std::cout << "Error: the number of scorers and results is different."
+              << std::endl;
+    return 1;
+  }
 
-    assert(result.size() == next.size());
-    for (size_t i = 0; i < result.size(); ++i) {
-      assert(result[i] == next[i]);
+  for (size_t i = 0; i < scored_results.size() - 1; ++i) {
+    if (scored_results[i].size() != scored_results[i + 1].size()) {
+      const std::string message = std::format(
+          "Error: the number of exams between "
+          "scorers is different: scorer {0}: {1} "
+          "elements, scorer {2}: {3} elements.",
+          i, scored_results[i].size(), i + 1, scored_results[i + 1].size());
+      std::cout << message << std::endl;
+      return 1;
+    }
+
+    for (size_t j = 0; j < scored_results[i].size(); ++j) {
+      if (scored_results[i][j] != scored_results[i + 1][j]) {
+        const std::string message = std::format(
+            "Error: the score of exam {0} is different: scorer {1}: {2}, "
+            "scorer {3}: {4}",
+            j, i, scored_results[i][j], i + 1, scored_results[i + 1][j]);
+        std::cout << message << std::endl;
+        return 1;
+      }
     }
   }
+
+  std::cout << "All tests passed." << std::endl;
   return 0;
 }
